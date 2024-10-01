1788367b3f2b2571703f061b20ce1049086e21bc
florence <loracodes3@gmail.com>
2024-10-01 19:59:21 +0300

    initial commit

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-const temp = 100;
+const temp = 25;
 if (temp >0 && temp <=30){
     console.log('The weather is GOOD');
 }
 else {
     console.log('The weather is BAD');
 }
+
+//the outout will be first condition because temp 25 is greater than 0 and less than 30
+//if the const temp was either below 0 or above 30, the output would be 'The weather is BAD' 
