3cc00d585ff348f21d0da2c50b2fc69c24df767f
florence <loracodes3@gmail.com>
2024-10-01 20:04:08 +0300

    or operator

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+     //AND OPERATOR
 const temp = 25;
 if (temp >0 && temp <=30){
     console.log('The weather is GOOD');
@@ -5,6 +6,14 @@ if (temp >0 && temp <=30){
 else {
     console.log('The weather is BAD');
 }
-
+//for it to be true, both conditions must pass
 //the outout will be first condition because temp 25 is greater than 0 and less than 30
 //if the const temp was either below 0 or above 30, the output would be 'The weather is BAD' 
+
+
+     //OR OPERATOR
+const degree =20;
+if (degree >0 || degree<=30){
+    console.log('The degrees are FAVOURIBLE')
+}
+
